8bc25782bc253eb21b3efa77149b3ecfd4d6a3e7
morteza-nouri <morteza1noori@gmail.com>
2023-03-18 02:39:51 +0330

    implement first phase

--- handsOn.go
+++ handsOn.go
@@ -2,21 +2,57 @@ package main
 
 import (
 	"fmt"
-	"strings"
+	"regexp"
+	"strconv"
+	"unicode"
 )
 
-func capitalize(sentence string) string {
-	result := ""
-	for index, char := range sentence {
-		if index == 0 && char != ' ' {
-			result += strings.ToUpper(string(char))
-		} else {
-			result += string(char)
+func capitalizeFirstLetter(sentence string) string {
+	mutableSentence := []rune(sentence)
+	for index, r := range mutableSentence {
+		if unicode.IsLetter(r) {
+			mutableSentence[index] = unicode.ToUpper(r)
+			break
 		}
 	}
-	return result
+	return string(mutableSentence)
+}
+
+func convertToOrdinal(num string) string {
+	var ordinalDictionary = map[int]string{
+		0: "th",
+		1: "st",
+		2: "nd",
+		3: "rd",
+		4: "th",
+		5: "th",
+		6: "th",
+		7: "th",
+		8: "th",
+		9: "th",
+	}
+	n, err := strconv.Atoi(num)
+	if err != nil {
+		panic(err)
+	}
+	return num + ordinalDictionary[n%10]
+}
+
+func ordinalizeNumbers(sentence string) string {
+	return regexp.MustCompile("[0-9]+").ReplaceAllStringFunc(sentence, convertToOrdinal)
+}
+
+func sentenceTokenizer(text string) []string {
+	return regexp.MustCompile("[^.!?]*[.!?]").FindAllString(text, -1)
 }
 
 func main() {
-	fmt.Println(capitalize("hello world!"))
+	text := "hello to my 1 attempt. this is Me!  give me the code.are you Señor? ? you bought 22, 23 and 25 of them."
+	sentences := sentenceTokenizer(text)
+	result := ""
+	for _, sentence := range sentences {
+		sentence = ordinalizeNumbers(sentence)
+		result += capitalizeFirstLetter(sentence)
+	}
+	fmt.Print(result)
 }
