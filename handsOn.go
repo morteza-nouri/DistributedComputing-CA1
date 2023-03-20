bc9bdd1c4df855d6479e19c4f1f0866ae8dedabc
morteza-nouri <morteza1noori@gmail.com>
2023-03-20 10:34:41 +0330

    read text from stdin

--- handsOn.go
+++ handsOn.go
@@ -1,13 +1,22 @@
 package main
 
 import (
+	"bufio"
 	"distributedComputing-CA1/grammar"
 	"fmt"
+	"log"
+	"os"
 )
 
 func main() {
-	text := "hello to my 1 attempt. this is Me!  give me the code.are you Señor? ? you bought 22, 23 and 25 of them."
-	sentences := grammar.SentenceTokenizer(text)
+	//text := "hello to my 1 attempt. this is Me!  give me the code.are you Señor? ? you bought 22, 23 and 25 of them."
+	scanner := bufio.NewScanner(os.Stdin)
+	scanner.Scan()
+	err := scanner.Err()
+	if err != nil {
+		log.Fatal(err)
+	}
+	sentences := grammar.SentenceTokenizer(scanner.Text())
 	result := ""
 	for _, sentence := range sentences {
 		sentence = grammar.OrdinalizeNumbers(sentence)
